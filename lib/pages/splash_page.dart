644f7ff255872203e1992580213d871c35c6b175
AbhiramR2562 <abhiram2562@gmail.com>
2024-02-06 21:51:31 +0530

    splash screen added

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:chess/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatefulWidget {
@@ -9,10 +12,43 @@ class SplashPage extends StatefulWidget {
 
 class _SplashPageState extends State<SplashPage> {
   @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 6), () {
+      // Navigate to the homepage
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const HomePage()));
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Image.asset(''),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 300),
+            SizedBox(
+              width: 200,
+              height: 220,
+              child: Image.asset(
+                'assets/images/chess-wallpaper.jpg',
+                fit: BoxFit.fill,
+              ),
+            ),
+            const SizedBox(height: 50),
+            const Text(
+              "CHESS",
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 46,
+                fontSize: 30,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
